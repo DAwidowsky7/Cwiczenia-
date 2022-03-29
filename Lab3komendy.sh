bd5f485e630a5b9645d342f8c9a2796c525a13ea
Dawid <fanciak@wp.pl>
2022-03-29 20:27:26 +0200

    Plik bash

---
+++ Lab3komendy.sh
@@ -0,0 +1,52 @@
+git commit
+git commit
+
+git checkout -b bugfix
+
+git checkout -b bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+
+git checkout bugFix
+git checkout c4
+
+
+git checkout bugFix^
+
+git checkout C1
+git branch -f main C6
+git branch -f bugFix bugFix~3
+
+git reset pushed~2
+
+git reset HEAD^
+git checkout pushed
+git revert HEAD
+
+git cherry-pick C3 C4 C7
+
+git rebase -i C1
+
+git rebase -i HEAD~2
+git commit --amend
+git rebase -i HEAD~2
+git rebase caption main
+
+git checkout main
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+
+git tag v0 C1
+git tag v1 C2
+git checkout v1
